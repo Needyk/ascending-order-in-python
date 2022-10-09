ed32a5d3ab039dcd2124656ff9c64dd1c596c672
Needyk <69422004+Needyk@users.noreply.github.com>
2022-10-09 20:50:33 +0530

    Update and rename main (3).py to ascending.py

--- ascending.py
+++ ascending.py
@@ -1,8 +1,8 @@
 import time
 Numbers = []
 while True:
-    a = input("to enter a number simply enter a number to finish enter q: ")
-    if 'q' not in a:
+    a = input("to enter a number simply enter a number to finish enter Q: ")
+    if 'Q' not in a:
         Numbers.append(a)
     else: 
         break
